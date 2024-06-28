7b04903d5a967fd413a6bda1db14ad63fbfa8e9a
MichaelP901 <93054968+MichaelP901@users.noreply.github.com>
2024-06-28 14:56:42 -0400

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         //mongodb connection string
-        const con = await mongoose.connect(process.env.mongo_URI, {
+        const con = await mongoose.connect(process.env.Mongo_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
